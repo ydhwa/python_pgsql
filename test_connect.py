5ab5e998848a60d857e397bbd75100009b99521a
ydhwa <ydhwa_18@naver.com>
2019-06-19 13:46:03 +0900

    Initial commit

---
+++ test_connect.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect(user='webdb', password='webdb', host='192.168.1.48', port='5432', database='webdb')
+
+except Exception as e:
+    print(f'error: {e}')
+finally:
+    conn and conn.close()
+
